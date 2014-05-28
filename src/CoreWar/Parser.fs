03fa4ffd3fc1abc0393efefe85e33cecbcf5baba
Simon Fowler <sf37@st-andrews.ac.uk>
2014-05-28 23:56:57 +0100

    Initial commit, with basic functionality
    * Support for basic arith / MOV / DAT instructions
    * Parser
    * VM infrastructure
    * Core dumping (but not yet visualisation)

---
+++ src/CoreWar/Parser.fs
@@ -0,0 +1,53 @@
+﻿module Parser
+
+open FParsec
+open Redcode
+open VM
+
+// Workaround for the generic weirdness
+type Parser<'t> = Parser<'t, unit>
+
+let (parseInstruction : Parser<Instr>) = 
+        (stringReturn "ADD" ADD)
+    <|> (stringReturn "SUB" SUB)
+    <|> (stringReturn "MUL" MUL)
+    <|> (stringReturn "DIV" DIV)
+    <|> (stringReturn "MOD" MOD)
+    <|> (stringReturn "JMP" JMP)
+    <|> (stringReturn "JMZ" JMZ)
+    <|> (stringReturn "JMN" JMN)
+    <|> (stringReturn "DJN" DJN)
+    <|> (stringReturn "SPL" SPL)
+    <|> (stringReturn "CMP" CMP)
+    <|> (stringReturn "SEQ" SEQ)
+    <|> (stringReturn "SNE" SNE)
+    <|> (stringReturn "SLT" SLT)
+    <|> (stringReturn "LDP" LDP)
+    <|> (stringReturn "STP" STP)
+    <|> (stringReturn "NOP" NOP)
+    <|> (stringReturn "MOV" MOV)
+    <|> (stringReturn "DAT" DAT)
+
+let (parseLocation : Parser<LocationField>) =
+    spaces >>.
+    ((anyOf "#@" >>= function
+          '#' -> pint32 |>> (fun x -> {AddrMode = Immediate ; Value = x} )
+        | '@' -> pint32 |>> (fun x -> {AddrMode = Indirect ; Value = x} )  
+     ) <|> (pint32 |>> (fun x -> {AddrMode = Direct ; Value = x} )))
+
+let (parseCommand : Parser<Command>) =
+    pipe3 parseInstruction parseLocation parseLocation 
+        (fun instr loc1 loc2 -> {Instruction = instr ; Loc1 = loc1 ; 
+                                 Loc2 = loc2} )
+
+let parseWarrior lines =
+    let name = List.head lines
+    let instrs = List.tail lines
+    // Parse each instruction individually
+    let map_fn x = 
+        printfn "Parsing %s" x
+        match run parseCommand x with
+            |  Success(result, _, _) -> result
+            | Failure(err, _, _) -> failwith err
+    let parsed_instrs = List.map map_fn instrs
+    { Name = name ; Code = parsed_instrs }
---
+++ src/CoreWar/Redcode.fs
@@ -0,0 +1,51 @@
+﻿module Redcode
+
+// Define the core instruction set
+type Instr = 
+    | DAT // Kill the process 
+    | MOV // Copy data from one address to another
+    | ADD // Add one number to another
+    | SUB // Subtract one number from another
+    | MUL // Multiply
+    | DIV // Divide: if /0, kill process
+    | MOD // Modulus
+    | JMP // Unconditional jump
+    | JMZ // Tests a number, jumps if 0
+    | JMN // Tests, jumps if *not* 0
+    | DJN // Decrement and jump if not 0
+    | SPL // Split -- start a second process at another address
+    | CMP // Compare two instructions, skip the next if they're equal
+    | SEQ // As above
+    | SNE // Skip if *not* equal
+    | SLT // Compares two values, and skips next if first lower than second
+    | LDP // Load from private storage space
+    | STP // Save to private storage space
+    | NOP // Do nothing
+    ;;
+
+type AddressingMode =
+    | Direct     // Addr relative to current PC
+    | Indirect   // B-field indirect: loc = (PC + val) + B-field of this instr
+    | Immediate  // Absolute value
+
+type LocationField = { AddrMode : AddressingMode; Value : int };;
+
+type Command = { Instruction : Instr; Loc1 : LocationField; Loc2 : LocationField };;
+
+let showLocation loc = 
+    let symb = 
+        match loc.AddrMode with
+              Direct -> ""
+            | Indirect -> "@"
+            | Immediate -> "#"
+    symb + loc.Value.ToString()
+
+let showCommand cmd = 
+    sprintf "%A %s %s" cmd.Instruction (showLocation cmd.Loc1) (showLocation cmd.Loc2)
+
+let printProgram prog = 
+    List.map(fun cmd -> printfn "%s" (showCommand cmd)) prog |> ignore
+
+let showProgram prog = 
+    Array.fold (fun acc instr -> acc + (showCommand instr) + "\n")
+        "" prog
